6df3c7faad7fe017caae43618ff2e03c3237e2b2
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-23 19:57:58 -0500

    Sliders

---
+++ guia_8/src/sliders/Sliders.java
@@ -0,0 +1,21 @@
+
+package sliders;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Sliders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
